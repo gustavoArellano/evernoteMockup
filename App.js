547791ac9c8a7662fabd0c36c2c68f6a14ab5fc2
Gustavo Arellano <gustavo813arellano@gmail.com>
2019-08-18 22:01:35 -0700

    commit

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ class App extends Component {
           data['id'] = _doc.id;
           return data;
         });
-        this.setState({ notes: notes})
+        this.setState({ notes: note})
       });
   }
 }
